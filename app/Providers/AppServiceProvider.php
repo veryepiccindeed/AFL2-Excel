4a074d0d681e0e08c8d21fa8a8f855f2c7476519
Asep <excelparinussa23@gmail.com>
2024-11-23 13:13:05 +0800

    Another perbaikan 17

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,9 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if(env('APP_ENV') !== 'local'){
-            URL::forceScheme('https');
-        }
+    
     }
 
     /**
